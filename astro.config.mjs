f30c9e28ba557f4f96dcddc7b89b3b6d2856400c
Sebastiano <s.licciardello.dev@proton.me>
2026-05-21 13:03:51 +0200

    fix: set correct base and site for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: process.env.SITE ?? 'https://rainbowsicilytrek.com',
-  base: process.env.BASE_PATH ?? '/'
+  site: process.env.SITE ?? 'https://sebastianolicciardello.github.io',
+  base: process.env.BASE_PATH ?? '/rainbow-sicily-trek/'
 });
